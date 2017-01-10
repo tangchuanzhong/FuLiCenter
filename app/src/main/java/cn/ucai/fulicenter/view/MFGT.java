a682d583afb234caaca411d775d2799b85136fc5
tangchuanzhong <1319902136@qq.com>
2017-01-10 15:03:02 +0800

    添加闪屏界面以及跳转动画

---
+++ app/src/main/java/cn/ucai/fulicenter/view/MFGT.java
@@ -0,0 +1,16 @@
+package cn.ucai.fulicenter.view;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * Created by Administrator on 2017/1/10 0010.
+ */
+public class MFGT {
+    public static void startActivity(Activity context,Class<?> clz){
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+        context.startActivity(new Intent(context,clz));
+    }
+}
