95aa1e87f0da61292c04eac6868aeda747f088e4
tangchuanzhong <1319902136@qq.com>
2017-01-17 14:43:12 +0800

    将个人信息在个人中心显示出来

--- app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
@@ -6,14 +6,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.utils.ImageLoader;
+import cn.ucai.fulicenter.view.MFGT;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PersonalCenterFragment extends Fragment {
 
+
+    @BindView(R.id.iv_user_avatar)
+    ImageView ivUserAvatar;
+    @BindView(R.id.tv_user_name)
+    TextView tvUserName;
 
     public PersonalCenterFragment() {
         // Required empty public constructor
@@ -24,7 +37,24 @@ public class PersonalCenterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_center, container, false);
+        View layout = inflater.inflate(R.layout.fragment_personal_center, container, false);
+        ButterKnife.bind(this, layout);
+        initData();
+        return layout;
     }
 
+    private void initData() {
+        User user = FuLiCenterApplication.getUser();
+        if (user != null) {
+            loadUserInfo(user);
+        } else {
+            MFGT.gotoLogin(getActivity());
+        }
+
+    }
+
+    private void loadUserInfo(User user) {
+        ImageLoader.downloadImg(getContext(),ivUserAvatar,user.getAvatarPath());
+        tvUserName.setText(user.getMuserNick());
+    }
 }
