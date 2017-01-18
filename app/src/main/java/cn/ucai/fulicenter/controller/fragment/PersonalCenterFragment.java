88af22ed1724197fe5883c374098787b1c73b489
tangchuanzhong <1319902136@qq.com>
2017-01-18 16:16:19 +0800

    商品详情收藏商品的操作

--- app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/PersonalCenterFragment.java
@@ -49,7 +49,7 @@ public class PersonalCenterFragment extends Fragment {
         if (user != null) {
             loadUserInfo(user);
         } else {
-            MFGT.gotoLogin(getActivity());
+           // MFGT.gotoLogin(getActivity());
         }
 
     }
--- app/src/main/java/cn/ucai/fulicenter/model/net/IModelGoods.java
+++ app/src/main/java/cn/ucai/fulicenter/model/net/IModelGoods.java
@@ -13,4 +13,5 @@ import cn.ucai.fulicenter.model.bean.MessageBean;
 public interface IModelGoods {
     void downData(Context context,int goodsId, OnCompleteListener<GoodsDetailsBean> listener);
     void isCollect(Context context, int goodsId, String username, OnCompleteListener<MessageBean> listener);
+    void setCollect(Context context,int goodsId,String username,int action,OnCompleteListener<MessageBean> listener);
 }
